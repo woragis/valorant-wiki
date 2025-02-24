2e241e0528c5bd503ee96fbb6b215d575af2d250
woragis <masteringthecode.woragis+github@gmail.com>
2025-02-24 17:48:03 -0300

    Update root pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function RootNotFound() {
   return (
-    <main>
+    <main className='not-found'>
       <h1>Page not found</h1>
+      <p>
+        Click <Link href='/'>Here</Link> to go to Home
+      </p>
     </main>
   )
 }
